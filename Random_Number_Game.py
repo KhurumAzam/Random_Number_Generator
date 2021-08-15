001823788810741e96a47e2e5c2701d55912e632
KhurumAzam <76566157+KhurumAzam@users.noreply.github.com>
2021-08-15 15:17:19 -0400

    Update Random_Number_Game.py

--- Random_Number_Game.py
+++ Random_Number_Game.py
@@ -2,7 +2,7 @@ import random
 
 def main():
     print("Welcome to the random number generator \n")
-    computer_number = int(computer_input(user_difficulty()))
+    computer_number = int(user_difficulty())
     user_number = int(user_input())
     while user_number != computer_number:
       if user_number > computer_number:
@@ -14,20 +14,32 @@ def main():
 
 def user_difficulty():
     user_input=0
-    while user_input < 1 or user_input > 4:
+    while int(user_input) < 1 or int(user_input) > 4:
         print("Pick a difficulty:")
         print("1. Easy (1-100)")
         print("2. Medium (1-500)")
         print("3. Hard (1-1000")
         print("4. Insane (1-2000)")
-        user_input=int(input())
-        if user_input < 1 or user_input > 4:
+        user_input=input()
+        if (user_input.isnumeric() == False):
             print("Pick a valid option from menu")
+            user_input = 0
+        elif (user_input.isnumeric() == True):
+            if(int(user_input) < 1 or int(user_input) > 4):
+                print("Pick a valid option from menu")
+                user_input=0
+        else:
+            print("Programmer has an error and needs a coffee break :(")
+            quit()
     return user_input
 
 def user_input():
     print("Pick a number: ")
-    return input()
+    number = input()
+    while number.isnumeric() == False:
+        print("This is invalid input (ex. fraction, letter, etc.), please pick a number: ")
+        number = input()
+    return number
 
 def computer_input(difficulty):
     if difficulty == 1:
@@ -40,8 +52,7 @@ def computer_input(difficulty):
         return random.randint(1, 2000)
     else:
         print("Programmer has an error and needs a coffee break :(")
-        return -1
+        quit()
 
 if __name__ == "__main__":
     main()
-
